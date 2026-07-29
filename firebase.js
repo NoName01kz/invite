d6b985870193e5a627fb6564a191f6340598c698
NoName01kz <noname01kz@gmail.com>
2026-07-30 00:39:02 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,49 +5,63 @@ import {
     doc,
     setDoc,
     getDoc,
-    updateDoc,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
 
 import {
     getAuth,
     signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
+    onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDYRprXsYiXHG0CplJ8hpa6kcTAxQiTjv8",
-    authDomain: "starinvite-4fc7f.firebaseapp.com",
-    projectId: "starinvite-4fc7f",
-    storageBucket: "starinvite-4fc7f.firebasestorage.app",
-    messagingSenderId: "747549267202",
-    appId: "1:747549267202:web:7432b06f76349101975e1b"
+
+apiKey: "AIzaSyDYRprXsYiXHG0Cpl8J8hpa6kcTAxQiTjv8",
+
+authDomain: "starinvite-4fc7f.firebaseapp.com",
+
+projectId: "starinvite-4fc7f",
+
+storageBucket: "starinvite-4fc7f.firebasestorage.app",
+
+messagingSenderId: "747549267202",
+
+appId: "1:747549267202:web:7432b06f76349101975e1b"
+
 };
 
-// Инициализация Firebase
+
+
 const app = initializeApp(firebaseConfig);
 
-// Firestore
+
+
 const db = getFirestore(app);
 
-// Authentication
+
 const auth = getAuth(app);
 
-// Экспорт
+
+
 export {
-    db,
-    auth,
 
-    doc,
-    getDoc,
-    setDoc,
-    updateDoc,
-    serverTimestamp,
+db,
 
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
+auth,
+
+doc,
+
+setDoc,
+
+getDoc,
+
+serverTimestamp,
+
+signInWithEmailAndPassword,
+
+onAuthStateChanged
+
 };
