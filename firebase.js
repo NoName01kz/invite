45c33dc08b6435058b9e4d0538e8f2e1d0c54ce8
NoName01kz <noname01kz@gmail.com>
2026-07-30 00:54:57 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,67 +1,81 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+// Firebase App
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 
+
+// Firestore
 import {
+
     getFirestore,
     doc,
     setDoc,
     getDoc,
     serverTimestamp
+
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 
+// Authentication
 import {
-    getAuth,
-    signInWithEmailAndPassword,
-    onAuthStateChanged
+
+    getAuth
+
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 
 
+// Конфигурация Firebase
+
 const firebaseConfig = {
 
-apiKey: "AIzaSyDYRprXsYiXHG0Cpl8J8hpa6kcTAxQiTjv8",
+    apiKey: "AIzaSyDYRprXsYiXHG0CplJ8hpa6kcTAxQiTjv8",
 
-authDomain: "starinvite-4fc7f.firebaseapp.com",
+    authDomain: "starinvite-4fc7f.firebaseapp.com",
 
-projectId: "starinvite-4fc7f",
+    projectId: "starinvite-4fc7f",
 
-storageBucket: "starinvite-4fc7f.firebasestorage.app",
+    storageBucket: "starinvite-4fc7f.firebasestorage.app",
 
-messagingSenderId: "747549267202",
+    messagingSenderId: "747549267202",
 
-appId: "1:747549267202:web:7432b06f76349101975e1b"
+    appId: "1:747549267202:web:7432b06f76349101975e1b"
 
 };
 
 
 
+// Запуск Firebase
+
 const app = initializeApp(firebaseConfig);
 
 
 
+// База данных
+
 const db = getFirestore(app);
 
+
+
+// Авторизация
 
 const auth = getAuth(app);
 
 
 
+// Экспорт
+
 export {
 
-db,
+    db,
 
-auth,
+    auth,
 
-doc,
+    doc,
 
-setDoc,
+    setDoc,
 
-getDoc,
+    getDoc,
 
-serverTimestamp,
-
-signInWithEmailAndPassword,
-
-onAuthStateChanged
+    serverTimestamp
 
 };
